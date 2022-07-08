35d9f5bf165894b3ea8043132133f0bea2b771ff
ediegodeorazio <diego_night95@hotmail.com>
2022-07-08 00:17:43 -0300

    first Update

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+
+function App() {
+  return ( 
+    <div className = "App">
+      <header>
+          <h1 className = "titulo"> Ultra Games<span> Tienda de juegos </span> </h1> 
+      </header> 
+      <div className = "nav-bg">
+        <nav className = "navegacionPrincipal contenedor">
+          <a href="##">Inicio</a> 
+          <a href="##">Sobre nosotros</a>
+          <a href="##">Clientes</a>
+          <a href="##">Contacto</a>
+        </nav>
+        <body>
+            Cuerpo de la página
+        </body>
+        <footer>
+          Ultra Games - Tienda de juegos - 2022 - "Todos los derechos reservados"
+        </footer>
+    
+      </div>
+    </div>);
+}
+
+export default App;
