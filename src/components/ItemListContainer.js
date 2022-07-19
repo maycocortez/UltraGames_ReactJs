9698f4fce08fcd595389aaa317a0fd965f9184b1
ediegodeorazio <diego_night95@hotmail.com>
2022-07-19 18:50:21 -0300

    Añadiendo contador con Boton

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -6,14 +6,12 @@ import { ItemCount } from './ItemCount';
 export const ItemListContainer = ({texto}) => {
     const onAdd = (unidades) => {
         console.log (`Compraste ${unidades} unidades`);
-        document.querySelector('.compraste').innerHTML = `Has comprado ${unidades} unidades`
     }
 
     return (    
         <>
             <Title greeting={texto} />
             <ItemCount initial={1} stock={10} onAdd={onAdd} />
-            <p className='compraste'></p>
         </>
     );
 } 
